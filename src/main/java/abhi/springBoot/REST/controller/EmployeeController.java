dc1332e48af619be946281ec08f1ba85f2611ce0
abhinavoffice <abhinav.agarwal@wynk.in>
2020-04-05 14:12:56 +0530

    Added Swagger Docs Code files and changes

--- src/main/java/abhi/springBoot/REST/controller/EmployeeController.java
+++ src/main/java/abhi/springBoot/REST/controller/EmployeeController.java
@@ -3,34 +3,45 @@ package abhi.springBoot.REST.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import abhi.springBoot.REST.dto.Employee;
 import abhi.springBoot.REST.service.EmployeeServiceImpl;
 import abhi.springBoot.REST.service.IEmployeeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @RestController
+@EnableSwagger2
+@Api(value="EmployeeControllerApi", produces= MediaType.APPLICATION_JSON_VALUE)
 public class EmployeeController {
 
 	@Autowired
 	IEmployeeService empServiceRef;
 //	EmployeeServiceImpl empServiceRef;
 	
-	@RequestMapping("/welcome")
-	public String welcomeEmployee() {
-		return "Hello Employee";
-	}
+//	@RequestMapping("/welcome")
+//	public String welcomeEmployee() {
+//		return "Hello Employee";
+//	}
 	
-	@RequestMapping("/employees")
+	@RequestMapping(method=RequestMethod.GET,value="/employees")
 	public List<Employee> getAllEmployees(){
 		return empServiceRef.getAllEmployee();
 	}
 	
-	@RequestMapping("/employees/{empName}")
+	@RequestMapping(method=RequestMethod.GET,value="/employees/{empName}")
+	@ApiOperation("Get Employee detail with specific name")
+	@ApiResponses(value= { @ApiResponse(code=200, message="OK", response=Employee.class)})
 	public Employee getEmployeeById(@PathVariable String empName) {
 		return empServiceRef.getEmployeeByName(empName);
 	}
---
+++ src/main/java/abhi/springBoot/Swagger/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package abhi.springBoot.Swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import abhi.springBoot.REST.controller.EmployeeController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@PropertySource("classpath:swagger.properties")
+@ComponentScan(basePackageClasses = EmployeeController.class)
+@Configuration
+public class SwaggerConfig {
+	
+	private static final String SWAGGER_API_VERSION = "1.0";
+	private static final String LICENSE_TEXT = "License";
+	private static final String TITLE = "Employee Rest API";
+	private static final String DESCRIPTION = "RESTful API for Employee";
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title(TITLE)
+				.description(DESCRIPTION)
+				.license(LICENSE_TEXT)
+				.version(SWAGGER_API_VERSION)
+				.build();
+	}
+	
+	@Bean
+	public Docket employeeApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
+				.pathMapping("/")
+				.select()
+				.paths(PathSelectors.regex("/api.*"))
+				.build();
+	}
+}
