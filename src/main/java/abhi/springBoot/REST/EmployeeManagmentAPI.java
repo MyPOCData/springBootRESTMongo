7d79e59636d35b38f42c60d48544b0de1997d33c
abhinavoffice <abhinav.agarwal@wynk.in>
2020-10-03 00:12:18 +0530

    Added Etag caching logic

--- src/main/java/abhi/springBoot/REST/EmployeeManagmentAPI.java
+++ src/main/java/abhi/springBoot/REST/EmployeeManagmentAPI.java
@@ -1,10 +1,19 @@
 package abhi.springBoot.REST;
 
+import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @SpringBootApplication
 public class EmployeeManagmentAPI {
+	
+	@Bean
+	public Filter shallowEtagFilter() {
+		return new ShallowEtagHeaderFilter();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagmentAPI.class, args);
